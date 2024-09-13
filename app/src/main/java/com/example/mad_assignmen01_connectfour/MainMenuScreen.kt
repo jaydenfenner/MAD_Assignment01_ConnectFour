774552d0b2fc017bb730795d93edd262d80671e7
Jayden Fenner <jaydenfen@gmail.com>
2024-09-13 12:03:22 +0800

    feat: add landscape layout for disc colours screen

--- app/src/main/java/com/example/mad_assignmen01_connectfour/MainMenuScreen.kt
+++ app/src/main/java/com/example/mad_assignmen01_connectfour/MainMenuScreen.kt
@@ -1,10 +1,13 @@
 package com.example.mad_assignmen01_connectfour
 
 import android.content.res.Configuration
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,6 +39,7 @@ fun MainMenu_Portrait(navController: NavHostController, shVm: ConnectFourViewMod
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
