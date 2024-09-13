f718da62a3ac4a3aa373d0061a9c3074f1b551ec
Jayden Fenner <jaydenfen@gmail.com>
2024-09-13 11:06:47 +0800

    feat: add 2P start screen horizontal layout

--- app/src/main/java/com/example/mad_assignmen01_connectfour/Start2PGameScreen.kt
+++ app/src/main/java/com/example/mad_assignmen01_connectfour/Start2PGameScreen.kt
@@ -3,8 +3,11 @@ package com.example.mad_assignmen01_connectfour
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,8 +27,7 @@ fun Start2PGameScreen(navController: NavHostController, shVm: ConnectFourViewMod
             Configuration.ORIENTATION_PORTRAIT ->
                 Start2PGame_Portrait(navController = navController, shVm = shVm)
             else ->
-//                Start2Game_Landscape(navController = navController, shVm = shVm)
-                Start2PGame_Portrait(navController = navController, shVm = shVm)
+                Start2Game_Landscape(navController = navController, shVm = shVm)
         }
     }
 }
@@ -35,12 +37,14 @@ fun Start2PGame_Portrait(navController: NavHostController, shVm: ConnectFourView
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(text = "Single Player Game")
         GamePlayerSelector(shVm = shVm, selectedProfile = shVm.twoPlayerProfileSelectionP1,
+            maxHeight = 1000.dp,
             defaultProfile = shVm.player1Profile,
             unavailableProfile = shVm.twoPlayerProfileSelectionP2,
             onProfileSelected = { shVm.twoPlayerProfileSelectionP1 = it },
@@ -48,13 +52,15 @@ fun Start2PGame_Portrait(navController: NavHostController, shVm: ConnectFourView
             selectedColor = shVm.leftPlayerDiskColour,
         )
         GamePlayerSelector(shVm = shVm, selectedProfile = shVm.twoPlayerProfileSelectionP2,
+            prompt = "Select Player 2 Profile",
+            maxHeight = 1000.dp,
             defaultProfile = shVm.player2Profile,
             unavailableProfile = shVm.twoPlayerProfileSelectionP1,
             onProfileSelected = { shVm.twoPlayerProfileSelectionP2 = it },
-            prompt = "Select Player 2 Profile",
             selectedColor = shVm.rightPlayerDiskColour
         )
         StartGameScreenButtons(
+            modifier = Modifier.padding(bottom = 20.dp),
             onStartStandard = { navController.navigate(Routes.GAME_2P_STANDARD_7_6) },
             onStartSmall = { navController.navigate(Routes.GAME_2P_SMALL_6_5) },
             onStartLarge = { navController.navigate(Routes.GAME_2P_LARGE_8_7) },
@@ -63,9 +69,46 @@ fun Start2PGame_Portrait(navController: NavHostController, shVm: ConnectFourView
 }
 
 @Composable
-fun Start2PGame_Landscape() {
-    Column(Modifier.fillMaxSize()) {
-        Text("Landscape")
+fun Start2Game_Landscape(navController: NavHostController, shVm: ConnectFourViewModel) {
+    Column(Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "Single Player Game")
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState())
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                GamePlayerSelector(shVm = shVm, selectedProfile = shVm.twoPlayerProfileSelectionP1,
+                    maxHeight = 400.dp,
+                    defaultProfile = shVm.player1Profile,
+                    unavailableProfile = shVm.twoPlayerProfileSelectionP2,
+                    onProfileSelected = { shVm.twoPlayerProfileSelectionP1 = it },
+                    prompt = "Select Player 1 Profile",
+                    selectedColor = shVm.leftPlayerDiskColour,
+                )
+                GamePlayerSelector(shVm = shVm, selectedProfile = shVm.twoPlayerProfileSelectionP2,
+                    prompt = "Select Player 2 Profile",
+                    maxHeight = 400.dp,
+                    defaultProfile = shVm.player2Profile,
+                    unavailableProfile = shVm.twoPlayerProfileSelectionP1,
+                    onProfileSelected = { shVm.twoPlayerProfileSelectionP2 = it },
+                    selectedColor = shVm.rightPlayerDiskColour
+                )
+            }
+            StartGameScreenButtons(
+                modifier = Modifier.weight(1f),
+                onStartStandard = { navController.navigate(Routes.GAME_2P_STANDARD_7_6) },
+                onStartSmall = { navController.navigate(Routes.GAME_2P_SMALL_6_5) },
+                onStartLarge = { navController.navigate(Routes.GAME_2P_LARGE_8_7) },
+            )
+        }
+
     }
 }
 
@@ -87,12 +130,12 @@ fun Start2PGame_Preview() {
 fun Start2PGame_Preview5Inch() {
     Start2PGame_Preview()
 }
-//@Preview(name = "5-inch Device Landscape",
-//    widthDp = previewHeightDp, heightDp = previewWidthDp, showBackground = true)
-//@Composable
-//fun Start2PGame_Preview5InchLand() {
-//    Start2PGame_Preview()
-//}
+@Preview(name = "5-inch Device Landscape",
+    widthDp = previewHeightDp, heightDp = previewWidthDp, showBackground = true)
+@Composable
+fun Start2PGame_Preview5InchLand() {
+    Start2PGame_Preview()
+}
 
 // TODO check if we need these, my friend got 100% with no tablet layouts
 //@Preview(name = "10-inch Tablet Portrait",
